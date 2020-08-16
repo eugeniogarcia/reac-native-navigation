909bcc3cfaed496742fb691e841a1fd8fdad3603
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-08-16 17:18:48 +0200

    ios specific api

---
+++ API Reference iOS Specific/Platform/Platform.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text, Platform } from 'react-native'
+
+const PlatformExample = () => (
+  <Text style={{ marginTop: 100, color: Platform.OS === 'ios' ? 'blue' : 'green' }}>
+     Hello { Platform.OS }
+  </Text>
+)
